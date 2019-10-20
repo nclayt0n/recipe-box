33330320d774f4e2856236aac2cc20a8fa7be235
Nicole Clayton <50276082+nclayt0n@users.noreply.github.com>
2019-10-20 17:47:04 -0400

    added component folders

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import {Switch,Route,Link} from 'react-router-dom'
 
-function App() {
-  return (
+class App extends React.Component {
+  render(){
+    return (
     <div className="App">
       <header className="App-header">
       <h1>Recipe Box</h1>
       </header>
     </div>
   );
+  }
+  
 }
 
 export default App;
