a822c686d7da9f33bac853dc09bd923310b0a1b4
Nicole Clayton <50276082+nclayt0n@users.noreply.github.com>
2019-10-22 19:52:29 -0400

    took out measurement from ingredients array, now its all within one object

--- src/STORE/STORE.js
+++ src/STORE/STORE.js
@@ -20,7 +20,7 @@ const STORE = {
             "name": "Apple Pie",
             "modified": "2019-01-03T00:00:00.000Z",
             "folderId": "b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1",
-            "ingredients": [{ name: "peach", measurement: { quantity: 1, unit: "cup" } }, { name: "sugar", measurement: { quantity: 1, unit: "cup" } }, { name: "crust", measurement: { quantity: 1, unit: "package" } }],
+            "ingredients": [{ name: "peach", quantity: 1, unit: "cup" }, { name: "sugar", quantity: 1, unit: "cup" }, { name: "crust", quantity: 1, unit: "package" }],
             "instructions": "Corporis accusamus placeat quas non voluptas. Harum fugit molestias qui. Velit ex animi reiciendis quasi. Suscipit totam delectus ut voluptas aut qui rerum. Non veniam eius molestiae rerum quam.\n \rUnde qui aperiam praesentium alias. Aut temporibus id quidem recusandae voluptatem ut eum. Consequatur asperiores et in quisquam corporis maxime dolorem soluta. Et officiis id est quia sunt qui iste reiciendis saepe. Ut aut doloribus minus non nisi vel corporis. Veritatis mollitia et molestias voluptas neque aspernatur reprehenderit..",
             "link": "www.notArealSite.com",
             "createdBy": "Oprah"
@@ -30,7 +30,7 @@ const STORE = {
             "name": "Pumpkin Pie",
             "modified": "2018-08-15T23:00:00.000Z",
             "folderId": "b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1",
-            "ingredients": [{ name: "pumpkin", measurement: { quantity: 2, unit: "cup" } }, { name: "sugar", measurement: { quantity: 1, unit: "cup" } }, { name: "crust", measurement: { quantity: 1, unit: "package" } }],
+            "ingredients": [{ name: "pumpkin", quantity: 2, unit: "cup" }, { name: "sugar", quantity: 1, unit: "cup" }, { name: "crust", quantity: 1, unit: "package" }],
             "instructions": "Corporis accusamus placeat quas non voluptas. Harum fugit molestias qui. Velit ex animi reiciendis quasi. Suscipit totam delectus ut voluptas aut qui rerum. Non veniam eius molestiae rerum quam.\n \rUnde qui aperiam praesentium alias. Aut temporibus id quidem recusandae voluptatem ut eum. Consequatur asperiores et in quisquam corporis maxime dolorem soluta. Et officiis id est quia sunt qui iste reiciendis saepe. Ut aut doloribus minus non nisi vel corporis. Veritatis mollitia et molestias voluptas neque aspernatur reprehenderit..",
             "link": "www.notArealSite.com",
             "created by": "Barb"
@@ -40,7 +40,7 @@ const STORE = {
             "name": "Peach Pie",
             "modified": "2018-03-01T00:00:00.000Z",
             "folderId": "b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1",
-            "ingredients": [{ name: "peach", measurement: { quantity: 1, unit: "cup" } }, { name: "sugar", measurement: { quantity: 1, unit: "cup" } }, { name: "crust", measurement: { quantity: 1, unit: "package" } }],
+            "ingredients": [{ name: "peach", quantity: 1, unit: "cup" }, { name: "sugar", quantity: 1, unit: "cup" }, { name: "crust", quantity: 1, unit: "package" }],
             "instructions": "Corporis accusamus placeat quas non voluptas. Harum fugit molestias qui. Velit ex animi reiciendis quasi. Suscipit totam delectus ut voluptas aut qui rerum. Non veniam eius molestiae rerum quam.\n \rUnde qui aperiam praesentium alias. Aut temporibus id quidem recusandae voluptatem ut eum. Consequatur asperiores et in quisquam corporis maxime dolorem soluta. Et officiis id est quia sunt qui iste reiciendis saepe. Ut aut doloribus minus non nisi vel corporis. Veritatis mollitia et molestias voluptas neque aspernatur reprehenderit..",
             "link": "www.notArealSite.com",
             "createdBy": "Mom"
@@ -50,7 +50,7 @@ const STORE = {
             "name": "Sweet Potato Pie",
             "modified": "2019-01-04T00:00:00.000Z",
             "folderId": "b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1",
-            "ingredients": [{ name: "sweet potatoes", measurement: { quantity: 3, unit: "cup" } }, { name: "sugar", measurement: { quantity: 1.5, unit: "cup" } }, { name: "crust", measurement: { quantity: 1, unit: "package" } }],
+            "ingredients": [{ name: "sweet potatoes", quantity: 3, unit: "cup" }, { name: "sugar", quantity: 1.5, unit: "cup" }, { name: "crust", quantity: 1, unit: "package" }],
             "instructions": "Corporis accusamus placeat quas non voluptas. Harum fugit molestias qui. Velit ex animi reiciendis quasi. Suscipit totam delectus ut voluptas aut qui rerum. Non veniam eius molestiae rerum quam.\n \rUnde qui aperiam praesentium alias. Aut temporibus id quidem recusandae voluptatem ut eum. Consequatur asperiores et in quisquam corporis maxime dolorem soluta. Et officiis id est quia sunt qui iste reiciendis saepe. Ut aut doloribus minus non nisi vel corporis. Veritatis mollitia et molestias voluptas neque aspernatur reprehenderit..",
             "link": "www.notArealSite.com",
             "createdBy": "Coolio"
