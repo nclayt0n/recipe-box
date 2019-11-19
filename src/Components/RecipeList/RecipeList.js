7e228d59b797062947723e6f01f867ee1ca51cff
Nicole Clayton <50276082+nclayt0n@users.noreply.github.com>
2019-11-19 18:12:38 -0500

    reciepes list changed fontsize

--- src/Components/RecipeList/RecipeList.js
+++ src/Components/RecipeList/RecipeList.js
@@ -4,17 +4,22 @@ import Context from '../../Context'
 import Header from '../Header/Header' 
 import Nav from '../Nav/Nav'
 import hpStyles from '../HomePage/HomePageStyles'
+import GetRecipeAndFolders from '../Network/GetRecipesAndFolders'
+import MediaQuery from 'react-responsive'
 import recipeListStyles from './RecipeListStyles'
 const uuidv4 = require('uuid/v4');
 class RecipeList extends React.Component{
     static contextType=Context;
     createDisplayedRecipes(recipes,style){
     return recipes.map(recipe=>{
-        return <li key={uuidv4()} style={style.recipeListStyle.li}>
-                    <Link to={`/recipe/${recipe.id}`} style={style.recipeListStyle.liA}>{recipe.name}</Link>
+        return <li key={uuidv4()} style={style.li}>
+                    <Link to={`/recipe/${recipe.id}`} style={style.liA}>{recipe.name}</Link>
                 </li>})}
 render(){
     let style;
+    if(this.context.recipes.length===0){
+           return  <GetRecipeAndFolders/>
+        }
     if(this.props.location.pathname===`/home-page`){
         style=hpStyles
     }if(this.props.location.pathname==='/recipe-list'){
@@ -26,16 +31,30 @@ render(){
         <>
         {(this.props.location.pathname!==`/home-page`)?<Header/>:''}
         {(this.props.location.pathname===`/home-page`)?'':<Nav/>}
+        <MediaQuery maxWidth={750} className='mobile'>
         <div className='RecipeList' style={style.recipeListStyle.div}>
             <ul style={style.recipeListStyle.ul}><Link to={'/recipe-list'} style={style.recipeListStyle.ulA}>RECIPES</Link> 
-            {this.createDisplayedRecipes(displayedRecipes,style)}
+            {this.createDisplayedRecipes(displayedRecipes,style.recipeListStyle)}
             </ul> 
            {(this.props.location.pathname===`/home-page`)?null: 
            (<>
-            <button><Link to={'/add-recipe'}>Add Recipe</Link></button>
-            <button type='button' onClick={()=>this.props.history.goBack()}>Back</button>
+            <button ><Link to={'/add-recipe'} style={style.recipeListStyle.button}>Add Recipe</Link></button>
+            <button type='button' onClick={()=>this.props.history.goBack()} style={style.recipeListStyle.button}>Back</button>
             </>)}
         </div>
+        </MediaQuery>
+        <MediaQuery minWidth={751} className='tablet'>
+        <div className='RecipeList' style={style.recipeListStyle.tablet.div}>
+            <ul style={style.recipeListStyle.ul}><Link to={'/recipe-list'} style={style.recipeListStyle.tablet.ulA}>RECIPES</Link> 
+            {this.createDisplayedRecipes(displayedRecipes,style.recipeListStyle.tablet)}
+            </ul> 
+           {(this.props.location.pathname===`/home-page`)?null: 
+           (<>
+            <button><Link to={'/add-recipe'} style={style.recipeListStyle.button}>Add Recipe</Link></button>
+            <button type='button' onClick={()=>this.props.history.goBack()} style={style.recipeListStyle.button}>Back</button>
+            </>)}
+        </div>
+        </MediaQuery>
         </>
     )
 }
--- src/Components/RecipeList/RecipeListStyles.js
+++ src/Components/RecipeList/RecipeListStyles.js
@@ -8,7 +8,7 @@ const recipeListStyles = {
             backgroundRepeat: 'noRepeat',
             backgroundSize: 'cover',
             backgroundColor: 'var(--brown)',
-            width: 'fit-content',
+            width: '80%',
             backgroundPosition: 'center',
             textAlign: 'center',
             display: 'flex',
@@ -25,18 +25,19 @@ const recipeListStyles = {
         },
         ul: {
             padding: '10px',
-            fontSize: '2.25vw',
+
             fontWeight: 'bold',
             cursor: 'pointer',
             color: 'white'
         },
         ulA: {
+            fontSize: '3vw',
             color: 'white',
             textDecoration: 'none'
         },
         li: {
             padding: '10px',
-            fontSize: '1.75vw',
+            fontSize: '2.25vw',
             cursor: 'pointer',
             listStyle: 'none',
             textAlign: 'center',
@@ -44,7 +45,63 @@ const recipeListStyles = {
         liA: {
 
             color: 'white',
-            textDecoration: 'none'
+            textDecoration: 'none',
+            fontSize: '2.5vw'
+        },
+        button: {
+            margin: '10px auto auto ',
+            color: 'var(--purple)',
+            backgroundColor: 'white',
+            fontWeight: 'bold',
+            width: 'fit-content'
+        },
+        tablet: {
+            div: {
+                backgroundImage: `url(${backgroundImage})`,
+                backgroundRepeat: 'noRepeat',
+                backgroundSize: 'cover',
+                backgroundColor: 'var(--brown)',
+                width: '50%',
+                backgroundPosition: 'center',
+                textAlign: 'center',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                margin: 'auto',
+                marginTop: '50px',
+                padding: '50px',
+                textDecoration: 'none',
+                listStyle: 'none',
+                border: '1px solid white'
+
+
+            },
+            ulA: {
+                fontSize: '1.75vw',
+                color: 'white',
+                textDecoration: 'none'
+            },
+            li: {
+                padding: '10px',
+                fontSize: '1.5vw',
+                cursor: 'pointer',
+                listStyle: 'none',
+                textAlign: 'center',
+            },
+            liA: {
+
+                color: 'white',
+                textDecoration: 'none',
+                fontSize: '1.5vw'
+            },
+            button: {
+                margin: '10px auto auto ',
+                color: 'var(--purple)',
+                backgroundColor: 'white',
+                fontWeight: 'bold',
+                width: 'fit-content'
+            },
+
         }
     },
 
