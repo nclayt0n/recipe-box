058bbac43c7509c7671fda82caa72724ca673233
Nicole Clayton <50276082+nclayt0n@users.noreply.github.com>
2019-12-01 18:50:41 -0500

    added description to landing page

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -2,11 +2,15 @@ import React from 'react'
 import {withRouter,Link}from 'react-router-dom'
 import landingPageStyles from './LandingPageStyles'
 import TokenService from '../../services/token-service'
+import MediaQuery from 'react-responsive'
 class LandingPage extends React.Component{
     render(){ 
         let style=landingPageStyles
-        return(
+        const description='allows you to create Recipes and store them in different Folders. It makes for an easier experience, by keeping all your meals, menus, and memories safe and organized in one place.'
+        
+        return(<> 
             <div className='landingPage' style={style.landingPage}>
+           
                 <div className='greeting' style={style.greeting}>
                     <div className='landingPageLinkBox' style={style.landingPageLinkBox}>
                         <button style={style.button}><Link to='/register' style={style.landingButtons} className='landingButtons'>Create User</Link></button>
@@ -14,6 +18,26 @@ class LandingPage extends React.Component{
                     </div>
                 </div>
             </div>
+            
+            <MediaQuery maxWidth={650}>
+                <section className='appDescription' style={style.descriptionBox}>
+                    <p style={style.description}><span id='RecipeBoxSpan' style={style.RecipeBoxSpan}>Recipe Box</span> {description} </p>
+                    <p style={style.descriptionStart}>To get started Create an Account, or Login.</p> 
+                </section>
+            </MediaQuery>
+            <MediaQuery minWidth={651} maxWidth={1000}>
+                <section className='appDescription' style={style.tablet.descriptionBox}>
+                    <p style={style.tablet.description}><span id='RecipeBoxSpan' style={style.tablet.RecipeBoxSpan}>Recipe Box</span> {description}</p>
+                    <p style={style.tablet.descriptionStart}>To get started Create an Account, or Login.</p> 
+                </section>
+            </MediaQuery>
+            <MediaQuery minWidth={1001}>
+                <section className='appDescription' style={style.laptop.descriptionBox}>
+                    <p style={style.laptop.description}><span id='RecipeBoxSpan' style={style.laptop.RecipeBoxSpan}>Recipe Box</span> {description}</p>
+                    <p style={style.laptop.descriptionStart}>To get started Create a new Account, or Login.</p> 
+                </section>
+            </MediaQuery>
+       </>
         )
     }
 }
