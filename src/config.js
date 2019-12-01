947dcdf0d47c3bd0dd98692455710df8051d7ce3
Nicole Clayton <50276082+nclayt0n@users.noreply.github.com>
2019-12-01 15:47:40 -0500

    otok off api_token-not in use and changed tile color in browser config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 module.exports = {
     API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT,
-    API_TOKEN: process.env.REACT_APP_API_TOKEN,
     TOKEN_KEY: 'recipebox-client-auth-token',
     JWT_EXPIRY: process.env.JWY_EXPIRY || '20s',
 }
